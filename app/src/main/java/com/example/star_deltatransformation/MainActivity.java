c64d3f7581ba5fbf43aef58356578c0546a5a313
SpPap <sppap01@gmail.com>
2020-08-10 16:45:29 +0300

    Fixed button's method

--- app/src/main/java/com/example/star_deltatransformation/MainActivity.java
+++ app/src/main/java/com/example/star_deltatransformation/MainActivity.java
@@ -9,12 +9,59 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private EditText edtTextRa, edtTextRb, edtTextRc, edtTextR1, edtTextR2, edtTextR3;
-    private Button clearButton;
+    private Button clearButton, calculateButton;
     private RadioButton wye_to_delta_rdButton, delta_to_wye_rdButton;
     private RadioGroup rdGroup;
     float Rd_1 = 0, Rd_2 = 0, Rd_3 = 0, Ry_1 = 0, Ry_2 = 0, Ry_3 = 0;
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.calculateButton:
+                float Rd_1 = 0, Rd_2 = 0, Rd_3 = 0;
+
+                if (wye_to_delta_rdButton.isChecked()) {
+
+                    wye_to_delta();
+
+                    edtTextRa.setText(String.valueOf(this.Rd_1));
+                    edtTextRb.setText(String.valueOf(this.Rd_2));
+                    edtTextRc.setText(String.valueOf(this.Rd_3));
+
+                }
+
+                if (delta_to_wye_rdButton.isChecked()) {
+
+                    delta_to_wye();
+
+                    edtTextRa.setText(String.valueOf(this.Ry_1));
+                    edtTextRb.setText(String.valueOf(this.Ry_2));
+                    edtTextRc.setText(String.valueOf(this.Ry_3));
+
+
+                }
+                break;
+
+            case R.id.clearButton:
+                String zero = "0.0";
+
+                edtTextR1.setText(zero);
+                edtTextR2.setText(zero);
+                edtTextR3.setText(zero);
+                edtTextRa.setText(zero);
+                edtTextRb.setText(zero);
+                edtTextRc.setText(zero);
+                break;
+
+            default:
+                break;
+
+
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,51 +77,17 @@ public class MainActivity extends AppCompatActivity {
         edtTextRc = findViewById(R.id.edtTextRc);
 
         clearButton = findViewById(R.id.clearButton);
+        calculateButton = findViewById(R.id.calculateButton);
 
         rdGroup = findViewById(R.id.rdGroup);
 
         wye_to_delta_rdButton = findViewById(R.id.wye_to_delta_rdButton);
         delta_to_wye_rdButton = findViewById(R.id.delta_to_wye_rdButton);
 
-
-    }
-
-
-    public void onClickCalcBtnMethod(View view) {
-        float Rd_1 = 0, Rd_2 = 0, Rd_3 = 0;
-
-        if (wye_to_delta_rdButton.isChecked()) {
-
-            wye_to_delta();
-
-            edtTextRa.setText(String.valueOf(this.Rd_1));
-            edtTextRb.setText(String.valueOf(this.Rd_2));
-            edtTextRc.setText(String.valueOf(this.Rd_3));
-
-        }
-
-        if (delta_to_wye_rdButton.isChecked()) {
-
-            delta_to_wye();
-
-            edtTextRa.setText(String.valueOf(this.Ry_1));
-            edtTextRb.setText(String.valueOf(this.Ry_2));
-            edtTextRc.setText(String.valueOf(this.Ry_3));
-
-        }
+        calculateButton.setOnClickListener(this);
+        clearButton.setOnClickListener(this);
 
 
-    }
-
-    public void onClickClrBtnMethod(View view){
-        String empty = " ";
-
-        edtTextR1.setText(empty);
-        edtTextR2.setText(empty);
-        edtTextR3.setText(empty);
-        edtTextRa.setText(empty);
-        edtTextRb.setText(empty);
-        edtTextRc.setText(empty);
     }
 
 
@@ -106,6 +119,5 @@ public class MainActivity extends AppCompatActivity {
         Ry_3 = Rd_1 * Rd_2 / denominator;
 
     }
-
 
 }
