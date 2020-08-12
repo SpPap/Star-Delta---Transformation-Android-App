4f7e205166eb2c51c369cee668b6f70782d7afa3
SpPap <sppap01@gmail.com>
2020-08-12 18:06:14 +0300

    Added Ohm Units spinner

---
+++ app/readme.md
@@ -0,0 +1,2 @@
+Android Application for Star(Wye)-to-Delta or Delta-to-Star(Wye) transformations.
+Useful app for Electrical Engineering courses!
--- app/src/main/java/com/example/star_deltatransformation/MainActivity.java
+++ app/src/main/java/com/example/star_deltatransformation/MainActivity.java
@@ -4,31 +4,45 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
+
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private EditText edtTextRa, edtTextRb, edtTextRc, edtTextR1, edtTextR2, edtTextR3;
     private Button clearButton, calculateButton;
     private RadioButton wye_to_delta_rdButton, delta_to_wye_rdButton;
     private RadioGroup rdGroup;
+    private Spinner R1_spinner, R2_spinner, R3_spinner;
+
+
     float Rd_1 = 0, Rd_2 = 0, Rd_3 = 0, Ry_1 = 0, Ry_2 = 0, Ry_3 = 0;
+    int unit_1 = 1, unit_2 = 1, unit_3 = 1;
+    String common_unit = "";
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
+
             case R.id.calculateButton:
-                float Rd_1 = 0, Rd_2 = 0, Rd_3 = 0;
+
 
                 if (wye_to_delta_rdButton.isChecked()) {
 
                     wye_to_delta();
 
-                    edtTextRa.setText(String.valueOf(this.Rd_1));
-                    edtTextRb.setText(String.valueOf(this.Rd_2));
-                    edtTextRc.setText(String.valueOf(this.Rd_3));
+                    edtTextRa.setText(String.valueOf(this.Rd_1) + " " + common_unit);
+                    edtTextRb.setText(String.valueOf(this.Rd_2) + " " + common_unit);
+                    edtTextRc.setText(String.valueOf(this.Rd_3) + " " + common_unit);
 
                 }
 
@@ -46,10 +60,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             case R.id.clearButton:
                 String zero = "0.0";
+                String empty = "";
 
-                edtTextR1.setText(zero);
-                edtTextR2.setText(zero);
-                edtTextR3.setText(zero);
+                edtTextR1.setText(empty);
+                edtTextR2.setText(empty);
+                edtTextR3.setText(empty);
                 edtTextRa.setText(zero);
                 edtTextRb.setText(zero);
                 edtTextRc.setText(zero);
@@ -87,15 +102,112 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         calculateButton.setOnClickListener(this);
         clearButton.setOnClickListener(this);
 
+        R1_spinner = findViewById(R.id.R1_spinner);
+        R2_spinner = findViewById(R.id.R2_spinner);
+        R3_spinner = findViewById(R.id.R3_spinner);
+
+
+        final HashMap<String, Integer> ohmUnitsMap = new HashMap<String, Integer>();
+        ohmUnitsMap.put("Ω", 1);
+        ohmUnitsMap.put("kΩ", 1000);
+        ohmUnitsMap.put("MΩ", 1000000);
+
+        //get keys (ohm units) from hashmap
+        Set<String> keysUnits = ohmUnitsMap.keySet();
+
+        final ArrayList<String> unitsList = new ArrayList<String>(keysUnits);
+
+        ArrayAdapter<String> ohmUnitsAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_spinner_dropdown_item,
+                unitsList
+        );
+
+        R1_spinner.setAdapter(ohmUnitsAdapter);
+        R2_spinner.setAdapter(ohmUnitsAdapter);
+        R3_spinner.setAdapter(ohmUnitsAdapter);
+
+        R1_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if (R1_spinner.getSelectedItem() == unitsList.get(0)) {
+                    unit_1 = ohmUnitsMap.get("Ω");
+                }
+                if (R1_spinner.getSelectedItem() == unitsList.get(1)) {
+                    unit_1 = ohmUnitsMap.get("kΩ");
+                }
+                if (R1_spinner.getSelectedItem() == unitsList.get(2)) {
+                    unit_1 = ohmUnitsMap.get("MΩ");
+                }
+
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        R2_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if (R2_spinner.getSelectedItem() == unitsList.get(0)) {
+                    unit_2 = ohmUnitsMap.get("Ω");
+                }
+                if (R2_spinner.getSelectedItem() == unitsList.get(1)) {
+                    unit_2 = ohmUnitsMap.get("kΩ");
+                }
+                if (R2_spinner.getSelectedItem() == unitsList.get(2)) {
+                    unit_2 = ohmUnitsMap.get("MΩ");
+                }
+
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        R3_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if (R3_spinner.getSelectedItem() == unitsList.get(0)) {
+                    unit_3 = ohmUnitsMap.get("Ω");
+                }
+                if (R3_spinner.getSelectedItem() == unitsList.get(1)) {
+                    unit_3 = ohmUnitsMap.get("kΩ");
+                }
+                if (R3_spinner.getSelectedItem() == unitsList.get(2)) {
+                    unit_3 = ohmUnitsMap.get("MΩ");
+                }
+
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        if ((R1_spinner.getSelectedItem().toString()).equals(R2_spinner.getSelectedItem().toString()) && (R1_spinner.getSelectedItem().toString()).equals(R3_spinner.getSelectedItem().toString())) {
+            common_unit = R1_spinner.getSelectedItem().toString();
+        }
 
     }
 
 
     public void wye_to_delta() {
 
-        float Ry_1 = Float.valueOf(edtTextR1.getText().toString());
-        float Ry_2 = Float.valueOf(edtTextR2.getText().toString());
-        float Ry_3 = Float.valueOf(edtTextR3.getText().toString());
+        float Ry_1 = unit_1 * Float.parseFloat(edtTextR1.getText().toString());
+        float Ry_2 = unit_2 * Float.parseFloat(edtTextR2.getText().toString());
+        float Ry_3 = unit_3 * Float.parseFloat(edtTextR3.getText().toString());
 
         float numerator = Ry_1 * Ry_2 + Ry_1 * Ry_3 + Ry_2 * Ry_3;
 
@@ -108,9 +220,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     public void delta_to_wye() {
 
-        float Rd_1 = Float.valueOf(edtTextR1.getText().toString());
-        float Rd_2 = Float.valueOf(edtTextR2.getText().toString());
-        float Rd_3 = Float.valueOf(edtTextR3.getText().toString());
+        float Rd_1 = unit_1 * Float.parseFloat(edtTextR1.getText().toString());
+        float Rd_2 = unit_2 * Float.parseFloat(edtTextR2.getText().toString());
+        float Rd_3 = unit_3 * Float.parseFloat(edtTextR3.getText().toString());
 
         float denominator = Rd_1 + Rd_2 + Rd_3;
 
@@ -119,5 +231,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Ry_3 = Rd_1 * Rd_2 / denominator;
 
     }
+
 
 }
